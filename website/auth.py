a6bd807489836068b9fc52e03f92c295b21e50f2
tinoco123 <tinocojona@outlook.com>
2022-06-29 15:49:32 -0500

    Authentication complete

--- website/auth.py
+++ website/auth.py
@@ -7,6 +7,9 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "GET":
+        if current_user.is_authenticated:
+            return redirect(url_for("views.home"))
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
@@ -21,7 +24,7 @@ def login():
                 flash("Incorrect password, try again.", category="error")
         else:
             flash("Email doesn't exist", category="error")
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
@@ -59,4 +62,4 @@ def sign_up():
             login_user(new_user, remember=True)
             flash("Account created successfully", category="success")
             return redirect(url_for("views.home"))
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
