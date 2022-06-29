edc986887ae5346479018b4deb2bcab38492c981
tinoco123 <tinocojona@outlook.com>
2022-06-29 14:06:20 -0500

    Database, models, and authentication

---
+++ website/models.py
@@ -0,0 +1,18 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(70))
+    notes = db.relationship("Note")
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    note = db.Column(db.String(1000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
