33129ca987a7ac4e900cd8e6bc7439fe0f34fec6
tinoco123 <tinocojona@outlook.com>
2022-06-29 00:26:58 -0500

    basic templates

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "f8ae4b6af41137f26af63cdbd05ceb58037aa598c2f74df719eaa72b38938199"
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
     return app
